e0e9b1abd29f49db75f4c01f23f85b6914c1e324
XoDeR <vldsyata@gmail.com>
2024-11-22 23:21:32 +0100

    added kinde for auth

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { KindeProvider } from "@kinde-oss/kinde-auth-react";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <KindeProvider
+      clientId="490c61e80939411bb5e149c3312ffc86"
+      domain="https://educator.kinde.com"
+      redirectUri="http://localhost:5173"
+      logoutUri="http://localhost:5173"
+    >
+      <App />
+    </KindeProvider>
+
   </StrictMode>,
 )
