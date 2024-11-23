ea2e8fa261d100cbdfa6ebf1a7c46c72a5e7d4e9
XoDeR <vldsyata@gmail.com>
2024-11-23 11:40:20 +0100

    added 2 buttons as components

---
+++ src/components/DeleteButton.tsx
@@ -0,0 +1,17 @@
+// @ts-nocheck
+import { useTodosContext } from "./../lib/hooks";
+
+export default function DeleteButton({ id }) {
+    const { deleteTodo } = useTodosContext();
+
+    return (
+        <button
+            onClick={(e) => {
+                e.stopPropagation();
+                deleteTodo(id);
+            }}
+        >
+            ❌
+        </button>
+    );
+}
