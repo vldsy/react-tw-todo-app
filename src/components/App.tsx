f229744ef0b24224b545c0cabea825c588a72c10
XoDeR <vldsyata@gmail.com>
2024-11-23 18:18:31 +0100

    added sidebar and styling to main to use grid

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="font-sans bg-[#f1d4b3] min-h-screen flex justify-center items-center flex-col">
       <BackgroundHeading />
-      <main>
+      <main className="relative w-[972px] shadow-[0_4px_4px_rgb(0,0,0,0.08)] h-[636px] bg-[#fff] rounded-[8px] overflow-hidden grid grid-cols-[7fr_4fr] grid-rows-[59px_1fr]">
         <Header />
         <TodoList />
         <Sidebar />
