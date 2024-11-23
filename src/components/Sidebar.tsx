4526c332bde2cf4592c1a289128fd820620ae8b9
XoDeR <vldsyata@gmail.com>
2024-11-23 18:04:32 +0100

    form to add a todo

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,7 @@
+import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
+import AddTodoForm from "./AddTodoForm";
+import Button from "./Button";
+
 export default function Sidebar() {
     return (
         <div className="col-[2_/_3] row-[2_/_3] bg-[#fffcf9] pt-[18px] px-[25px] pb-[28px] flex flex-col justify-between border-l border-l-[rgba(0,0,0,0.08)]">Sidebar</div>
